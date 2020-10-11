198493578816001d0a08b196699dc9f4bf7db804
jborza <jborza@gmail.com>
2020-10-11 08:56:29 +0200

    fixed rom generator - two 128x32 slices

--- generate_rom.py
+++ generate_rom.py
@@ -1,11 +1,14 @@
 from PIL import Image
 # assuming 1bpp 64x128 png
-im = Image.open('che2.png')
-# read pixels by bytes
+im = Image.open('lenna.png')
+
 x=0
 y=0
-for y in range(0,64):
-    #get 8 bits
+
+
+for y in range(0,32):
+    #first row
+    #get 8 bits at a time
     for x in range(0,128,8):
         byte = 0
         for x_offset in range(0,8):
@@ -13,5 +16,13 @@ for y in range(0,64):
                 byte = byte << 1
             px = im.getpixel((x+x_offset,y))
             byte = byte | px
-        #   print(byte)
+        print(f'{byte:08b}')
+    #second row
+    for x in range(0,128,8):
+        byte = 0
+        for x_offset in range(0,8):
+            if(x_offset > 0):
+                byte = byte << 1
+            px = im.getpixel((x+x_offset,y+32))
+            byte = byte | px
         print(f'{byte:08b}')
